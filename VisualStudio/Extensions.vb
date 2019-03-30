e1a5cec0c02d30cdee1cf7faee8b93e7c6ab60dd
xieguigang <xie.guigang@gcmodeller.org>
2019-03-30 11:33:35 +0800

    update file headers

--- VisualStudio/Extensions.vb
+++ VisualStudio/Extensions.vb
@@ -52,18 +52,9 @@ Public Module Extensions
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     <Extension>
     Public Function EnumerateSourceFiles(vbproj As String) As IEnumerable(Of String)
-        Return vbproj.LoadXml(Of Project).EnumerateSourceFiles
-    End Function
-
-    ''' <summary>
-    ''' Enumerate all of the vb source files in this vbproj.
-    ''' </summary>
-    ''' <param name="vbproj"></param>
-    ''' <returns></returns>
-    <MethodImpl(MethodImplOptions.AggressiveInlining)>
-    <Extension>
-    Public Function EnumerateSourceFiles(vbproj As Project) As IEnumerable(Of String)
-        Return vbproj.ItemGroups _
+        Return vbproj _
+            .LoadXml(Of Project) _
+            .ItemGroups _
             .Where(Function(items) Not items.Compiles.IsNullOrEmpty) _
             .Select(Function(items)
                         Return items.Compiles _
